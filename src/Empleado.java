592c9555f742cafda102224bd98606b500c27ac0
casoch <casoch@gmail.com>
2025-03-09 13:41:19 +0100

    Comiteando desde eclipse

--- src/Empleado.java
+++ src/Empleado.java
@@ -1,4 +1,15 @@
 public class Empleado {
     private String nombre;
     private double sueldo;
+    
+    public Empleado()
+    {
+    	
+    }
+    
+    public Empleado(String nombre, double sueldo)
+    {
+    	this.nombre = nombre;
+    	this.sueldo = sueldo;
+    }
 }
