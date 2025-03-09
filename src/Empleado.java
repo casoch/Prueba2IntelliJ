53e45200e71ae591eb3266ee7bc58b18b91eda37
casoch <casoch@gmail.com>
2025-03-09 13:47:38 +0100

    cosas de empleado desde code

--- src/Empleado.java
+++ src/Empleado.java
@@ -1,15 +1,18 @@
 public class Empleado {
     private String nombre;
     private double sueldo;
+    private int medallitas;
     
     public Empleado()
     {
-    	
+        medallitas = 1;
     }
     
     public Empleado(String nombre, double sueldo)
     {
+        this();
     	this.nombre = nombre;
     	this.sueldo = sueldo;
     }
+
 }
